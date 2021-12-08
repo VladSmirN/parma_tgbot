26e73ddeacddb1cf3b0d3be6801105c4cadca29d
vlad <yavlads2014@yandex.ru>
2021-12-08 21:07:45 +0500

    разделение сред разработки dev/prod

--- data/config.py
+++ data/config.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 PORT = 8877
+PROD_OR_DEV = os.environ["PROD_OR_DEV"]
 BOT_TOKEN = os.environ["bot_token"]
 NGROK_TOKEN = os.environ["NGROK_TOKEN"]
 WEBHOOK_PATH = f'/tg/webhooks/bot/{BOT_TOKEN}'
@@ -10,8 +11,15 @@ LOGS_BASE_PATH = str(Path(__file__).parent.parent / 'logs')
 
 admins = []
 
+if PROD_OR_DEV == 'prod':
+    #запуск в докере
+    HOSTNAME = 'mongo'
+else:
+    #запуск на локальной машине
+    HOSTNAME = 'localhost'
+
 mongo = {
-    'hostname':  'mongo',
+    'hostname':  HOSTNAME,
     'password': 'root',
     'username':  'example',
     'port': 27017,
