b49d46468cf4e38d1b8e0b9381be58abaa7c1d47
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-12-16 13:08:59 +0500

    Поправил ссыль

--- handlers/user/about_company.py
+++ handlers/user/about_company.py
@@ -3,6 +3,6 @@ from aiogram import types
 
 async def about_company(msg: types.Message):
     txt = ['Российский разработчик ПО на основе современных информационных технологий',
-           '<a>https://parma.team/#rec258373195</a>',
+           '<a>https://parma.team</a>',
            ]
     await msg.answer('\n'.join(txt))
