996665c03062a2806cd723fb0fa01a0553669074
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-12-16 13:09:11 +0500

    Поменял help

--- handlers/user/help.py
+++ handlers/user/help.py
@@ -6,8 +6,9 @@ from utils.misc import rate_limit
 @rate_limit(5, 'help')
 async def bot_help(msg: types.Message):
     text = [
-        'Список команд: ',
-        '/start - Начать диалог',
-        '/help - Получить справку'
+        'Взаимодействие с ботом основано на кнопках.'
+        'Для связи с разработчиками:',
+        'https://t.me/rgbvpn',
+        'https://t.me/VladSmirN'
     ]
     await msg.answer('\n'.join(text))
