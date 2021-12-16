43524d79a2b095348772f2086ab2c60a5e24923c
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-12-16 13:21:58 +0500

    Update help.py

--- handlers/user/help.py
+++ handlers/user/help.py
@@ -1,14 +1,13 @@
 from aiogram import types
-
 from utils.misc import rate_limit
 
 
 @rate_limit(5, 'help')
 async def bot_help(msg: types.Message):
     text = [
-        'Взаимодействие с ботом основано на кнопках.'
+        'Взаимодействие с ботом полностью основано на кнопках. '
+        'Вам не нужно использовать какие-либо команды',
         'Для связи с разработчиками:',
-        'https://t.me/rgbvpn',
-        'https://t.me/VladSmirN'
+        'https://t.me/rgbvpn'
     ]
     await msg.answer('\n'.join(text))
